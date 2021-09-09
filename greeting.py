2aed03e824a2b5c6ea3c694dc3ac22b568dd6154
clong <clong389@gmail.com>
2021-09-09 17:10:13 -0400

    adding 5 new practice files

---
+++ greeting.py
@@ -0,0 +1,4 @@
+hello = "Hello"
+name = "Megan"
+greeting = hello + " " + name
+print(greeting)
---
+++ sports-weather.py
@@ -0,0 +1,10 @@
+temperature = 75
+
+if temperature > 80:
+    print("It's too hot.")
+    print("Stay inside today.")
+elif temperature < 60:
+    print("It's too cold.")
+    print("Let's make soup.")
+else:
+    print("Head outside, the weather is fine.")
