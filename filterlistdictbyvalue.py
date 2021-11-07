c499bfa854a783df6a9a4e534cc7573e4e46c8bd
clongcurious1 <84348727+clongcurious1@users.noreply.github.com>
2021-11-07 15:37:22 -0500

    discovered a problem: as dictreader converts the csv file rows into separate dictionaries, it is adding an extra space in the sex column so that the values written into the program are 'f ' or 'm ' rather than 'f' or 'm'. Cannot find anything online to explain why this happens or how to fix it - so I have commented out that code for now

--- filterlistdictbyvalue.py
+++ filterlistdictbyvalue.py
@@ -4,10 +4,10 @@ from csv import DictReader
 
 value1 = 'Samantha'
 value2 = 'n'
-value3 = 'm'
-value4 = 'd'
-value5 = 'sp'
-value6 = 'm'
+value3 = 'l'
+value4 = 's'
+value5 = 'sg'
+value6 = 'f'
 
 #to print 100dogs as list of dictionaries
 with open('csv.dogs100dictprep.csv', 'r') as read_obj:
@@ -15,10 +15,10 @@ with open('csv.dogs100dictprep.csv', 'r') as read_obj:
     dict_reader = DictReader(read_obj)
     #get a list of dictionaries from dict_reader
     list_of_dict = list(dict_reader)
-    print(list_of_dict)
+    #print(list_of_dict)
 
 #start matching against user prerequisites
-#rename list_of_dict
+#rename list_of_dict *you can rename a variable anywhere in the program
 petstoadopt = list_of_dict
 print('\nHere is the full list of available pets:\n')
 print(petstoadopt)
@@ -34,9 +34,9 @@ print(newList1)
 time.sleep(2)
 
 #How many pet dictionaries are in the new list?
-#Use list comprehension and isinstance()
-result = len([ele for ele in newList1 if isinstance(ele, dict)])
-print("\nSPECIAL NEEDS: The number of matching pets is " + str(result))
+#Use list comprehension and isinstance() to count # of dicts in new list
+result1 = len([ele for ele in newList1 if isinstance(ele, dict)])
+print("\nSPECIAL NEEDS: The number of matching pets is " + str(result1))
 time.sleep(3)
 
 #Round 2 Pet Match: Best Size (value3)
@@ -50,8 +50,8 @@ time.sleep(2)
 
 #How many pet dictionaries are in the new list?
 #Use list comprehension and isinstance()
-result = len([ele for ele in newList2 if isinstance(ele, dict)])
-print("\nSpecial Needs + Best Size: NEW number of matching pets is " + str(result))
+result2 = len([ele for ele in newList2 if isinstance(ele, dict)])
+print("\nSpecial Needs + Best Size: NEW number of matching pets is " + str(result2))
 time.sleep(2)
 
 #Round 3 Pet Match: Life Stage (value4)
@@ -65,8 +65,8 @@ time.sleep(2)
 
 #How many pet dictionaries are in the new list?
 #Use list comprehension and isinstance()
-result = len([ele for ele in newList3 if isinstance(ele, dict)])
-print("\nSpecial Needs, Best Size, Life Stage: NEW number of matching pets is " + str(result))
+result3 = len([ele for ele in newList3 if isinstance(ele, dict)])
+print("\nSpecial Needs, Best Size, Life Stage: NEW number of matching pets is " + str(result3))
 time.sleep(2)
 
 #Round 4 Pet Match: Social Needs (value5)
@@ -80,21 +80,24 @@ time.sleep(2)
 
 #How many pet dictionaries are in the new list?
 #Use list comprehension and isinstance()
-result = len([ele for ele in newList4 if isinstance(ele, dict)])
-print("\nSpecial Needs, Best Size, Life Stage, Social Needs: Number of matching pets is " + str(result))
+result4 = len([ele for ele in newList4 if isinstance(ele, dict)])
+print("\nSpecial Needs, Best Size, Life Stage, Social Needs: Number of matching pets is " + str(result4))
 time.sleep(2)
 
+#Commented this out lines 87-101 because when the file is being read
+#the dict.reader is printing the value of key: sex with an extra space
+# extra space "f " or "m " inside quotes is crashing the program
 #Round 5 Pet Match: Sex (value6)
 #use a dictionary comprehension to match sex:value6
-print ("\nFILTER: Match pets against user's 'sex' answer: \n" )
+#print ("\nFILTER: Match pets against user's 'sex' answer: \n" )
 #filter by list comprehension to dreate a new list of pet dictionaries
-keyValList = [str(value6)] #value you are matching against
-newList5 = [d for d in newList4 if d['sex'] in keyValList]
-print(newList5)
-time.sleep(2)
+#keyValList = [str(value6)] #value you are matching against
+#newList5 = [d for d in newList4 if d['sex'] in keyValList]
+#print(newList5)
+#time.sleep(2)
 
-#How many pet dictionaries are in the new list?
+##How many pet dictionaries are in the new list?
 #Use list comprehension and isinstance()
-result = len([ele for ele in newList5 if isinstance(ele, dict)])
-print("\nSpecial Needs, Best Size, Life Stage, Social Needs, Sex: Number of matching pets is " + str(result))
-time.sleep(2)
+#result5 = len([ele for ele in newList5 if isinstance(ele, dict)])
+#print("\nSpecial Needs, Best Size, Life Stage, Social Needs, Sex: Number of matching pets is " + str(result5))
+#time.sleep(2)
